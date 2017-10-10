b1c927a5de16536b24e8265239fdb283bd76234e
Juan Carlos Labrandero <charly@localhost.localdomain>
2017-10-10 17:24:05 -0400

    Primer archivo del plgin de MAGIS

---
+++ wp-content/plugins/magis/magis.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Magis
+ * Plugin URI: https://magis.com/
+ * Description: Gestión de citas y usuarios para la empresa constructora MAGIS.
+ * Version: 0.1.1
+ * Author: Juan Carlos Labrandero
+ * Author URI: https://magis.com
+ * Requires at least: 4.4
+ * Tested up to: 4.8
+ *
+ * Text Domain: magis
+ * Domain Path: /i18n/languages/
+ *
+ * @package Magis
+ * @category Core
+ * @author Juan Carlos Labrandero
+ */
